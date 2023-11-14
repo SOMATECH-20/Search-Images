98f430c1350fe76782e67e7955d817de8969762a
SOMATECH-20 <132387616+SOMATECH-20@users.noreply.github.com>
2023-11-14 11:48:17 +0300

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-// const ApiKey = "Your Unsplash ApikKey here";
-const ApiKey = "bpq0ZVcqNvc5d31UI6R8jmUfEs8wY-J8xvjyp69PvuI";
+const ApiKey = "Your Unsplash ApikKey here";
 
 const searchForm = document.getElementById('Search-form');
 const searchBox = document.getElementById('Search');
